1fb6b5fd5bcf120ff90bd3cf885e3149aa4ce099
Samit Koyom <samitkoyom@gmail.com>
2018-07-26 10:20:36 +0700

    update login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
+import { WebapiServiceProvider } from '../../providers/webapi-service/webapi-service';
 
 @IonicPage()
 @Component({
@@ -9,27 +10,53 @@ import { TabsPage } from '../tabs/tabs';
 })
 export class LoginPage {
 
-  username:string;
-  password:string;
+  userData = {
+    "username": "",
+    "password": ""
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  // ตัวแปรรับข้อมูลจาก api
+  responseData: any;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public webapi: WebapiServiceProvider,
+    public alertCtrl: AlertController) {
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
 
-  login()
-  {
-    if(this.username == "admin" && this.password == "1234"){
-      alert("เข้าระบบถูกต้อง");
-      // บันทึกข้อมูลลง Local Storage
-      localStorage.setItem("userData",this.username);
-      // ปิดหน้า login แล้วกลับไปหน้าหลัก
-      this.navCtrl.setRoot(TabsPage);
-    }else{
-      alert("ข้อมูลเข้าระบบไม่ถูกต้อง");
-    }
+  login() {
+
+    this.webapi.postData(this.userData,'login.php').then((result)=>{
+      this.responseData = result;
+      //alert(JSON.stringify(this.responseData));
+      if(this.responseData.userData){ // login success
+        // แสดงหน้าต่างแจ้งผู้ใช้
+        let alert = this.alertCtrl.create({
+          title: "สถานะการเข้าระบบ",
+          subTitle:"เข้าระบบเรียบร้อยแล้ว",
+          buttons: ['Dismiss']
+        });
+        alert.present();
+        // บันทึกข้อมูลลง Local Storage
+        localStorage.setItem("userData", this.userData.username);
+        // ปิดหน้า login แล้วกลับไปหน้าหลัก
+        this.navCtrl.setRoot(TabsPage);
+      }else{ // login fail
+        // แสดงหน้าต่างแจ้งผู้ใช้
+        let alert = this.alertCtrl.create({
+          title: "สถานะการเข้าระบบ",
+          subTitle:"เข้าสู่ระบบไม่สำเร็จ",
+          buttons: ['Dismiss']
+        });
+        alert.present();
+      }
+    });
   }
 
 }
